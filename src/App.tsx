5d18c5a0be38b9ce29384950be31c425a029e26d
rokibulslam <rokibulslam6@gmail.com>
2025-01-09 22:41:48 +0600

    [revert]: revert to react 18

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,9 @@ const App: React.FC = () => {
 
   return (
     <div className="w-screen">
-      <h1 className="text-center">3D Model Viewer with AR</h1>
+      <h1 className="text-center my-10 text-3xl font-serif font-bold">
+        3D Model Viewer with AR
+      </h1>
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="w-full">
           <ModelViewer
@@ -25,14 +27,28 @@ const App: React.FC = () => {
             color={selectedColor}
           />
           <div className="w-full flex justify-center">
-            <button onClick={() => handleColorChange("1, 0, 0, 1")}>Red</button>
-            <button onClick={() => handleColorChange("0, 1, 0, 1")}>
+            <button
+              className="bg-red-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChange("1, 0, 0, 1")}
+            >
+              Red
+            </button>
+            <button
+              className="bg-green-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChange("0, 1, 0, 1")}
+            >
               Green
             </button>
-            <button onClick={() => handleColorChange("0, 0, 1, 1")}>
+            <button
+              className="bg-blue-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChange("0, 0, 1, 1")}
+            >
               Blue
             </button>
-            <button onClick={() => handleColorChange("1, 1, 0, 1")}>
+            <button
+              className="bg-yellow-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChange("1, 1, 0, 1")}
+            >
               Yellow
             </button>
           </div>
@@ -45,16 +61,28 @@ const App: React.FC = () => {
             color={selectedColorChair}
           />
           <div className="w-full flex justify-center">
-            <button onClick={() => handleColorChangeChair("1, 0, 0, 1")}>
+            <button
+              className="bg-red-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChangeChair("1, 0, 0, 1")}
+            >
               Red
             </button>
-            <button onClick={() => handleColorChangeChair("0, 1, 0, 1")}>
+            <button
+              className="bg-green-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChangeChair("0, 1, 0, 1")}
+            >
               Green
             </button>
-            <button onClick={() => handleColorChangeChair("0, 0, 1, 1")}>
+            <button
+              className="bg-blue-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChangeChair("0, 0, 1, 1")}
+            >
               Blue
             </button>
-            <button onClick={() => handleColorChangeChair("1, 1, 0, 1")}>
+            <button
+              className="bg-yellow-100 text-black px-4 py-1 rounded-md mr-2"
+              onClick={() => handleColorChangeChair("1, 1, 0, 1")}
+            >
               Yellow
             </button>
           </div>
--- src/declaration.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@google/model-viewer" {
-  // Optional: Add any known types or leave it as `any` for now
-  const ModelViewer: any;
-  export default ModelViewer;
-}
